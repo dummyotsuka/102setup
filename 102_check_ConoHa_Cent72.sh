0afd5ae7e76a6bcde36a6b4bd23919a42a464073
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-07-03 15:33:55 +0900

    Update 102_check_ConoHa_Cent72.sh

--- 102_check_ConoHa_Cent72.sh
+++ 102_check_ConoHa_Cent72.sh
@@ -21,11 +21,13 @@ echo  LAB5_syslog_start
 echo  LAB5_syslog_start 1>&2
 
 sed -i -e s/#$ModLoad imudp/$ModLoad imudp/ /etc/rsyslog.conf
+sed -i -e s/#$UDPServerRun 514/$UDPServerRun 514/ /etc/rsyslog.conf
 touch  /var/log/test.log
 echo "user.*                                                  /var/log/test.log" >> /etc/rsyslog.conf
 systemctl restart rsyslog
 #tail  -f  /var/log/test.log
 cat /var/log/test.log
+
 
 exec 1>> $gamen
 exec 2>> $gamen
